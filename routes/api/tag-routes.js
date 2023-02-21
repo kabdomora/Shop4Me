9dae9ce0c61f97e8e64e9549412a71955a783676
kabdomora <114969831+kabdomora@users.noreply.github.com>
2023-02-20 21:06:17 -0800

    tag post route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -38,7 +38,14 @@ router.get('/:id', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  // create a new tag
+  try {
+    const newTag = await Tag.create(req.body);
+
+    res.status(200).json(newTag);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 router.put('/:id', async (req, res) => {
