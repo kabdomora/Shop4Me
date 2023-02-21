54f78acd613e4f47a8336a18a054206f064c71f0
kabdomora <114969831+kabdomora@users.noreply.github.com>
2023-02-20 20:54:15 -0800

    tag get all route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,25 +3,37 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+router.get('/', async (req, res) => {
+  try {
+    const tagData = await Tag.findAll({
+      include: [{
+        model: Product,
+        through: ProductTag,
+      }],
+    });
+
+    const tag = tagData.get({ plain: true})
+    res.json(tag);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
 });
 
